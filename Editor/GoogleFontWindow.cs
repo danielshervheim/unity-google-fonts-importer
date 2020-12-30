a0af17ab37311468ef7e438436381d7fb588300a
Daniel Shervheim <danielshervheim@gmail.com>
2020-12-30 15:25:08 -0600

    cleaned up and commented

---
+++ Editor/GoogleFontWindow.cs
@@ -0,0 +1,110 @@
+// Copyright © Daniel Shervheim, 2020
+// www.danielshervheim.com
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+using EditorCoroutineUtility = Unity.EditorCoroutines.Editor.EditorCoroutineUtility;
+
+namespace DSS.GoogleFonts
+{
+    public class GoogleFontWindow : EditorWindow
+    {
+        // The font associated with this window.
+        GoogleFont font = null;
+
+        // The scroll position within the window.
+        Vector2 scrollPos = Vector2.zero;
+
+        // Returns a new window for the given font asset.
+        public static void Window(GoogleFont font)
+        {
+
+            GoogleFontWindow window = (GoogleFontWindow)EditorWindow.GetWindow(typeof(GoogleFontWindow), true, font.family);
+            window.Setup(font);
+        }
+
+        void Setup(GoogleFont font)
+        {
+            this.font = font;
+        }
+
+        void OnGUI()
+        {
+            if (font == null)
+            {
+                return;
+            }
+
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginVertical();
+            scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+            {
+                GUILayout.Label(font.family, EditorStyles.boldLabel);
+                EditorGUILayout.Space();
+
+                Dictionary<string, string> styles = font.GetStyles();
+
+                // Show download buttons for each specific style.
+                foreach (string style in styles.Keys)
+                {
+                    if (GUILayout.Button(style))
+                    {
+                        string path = EditorUtility.SaveFilePanelInProject("Download " + font.family + " (" + style + ")", font.family + " (" + style + ").ttf", "ttf", "Save font style as...");
+                        if (path.Length != 0)
+                        {
+                            EditorCoroutineUtility.StartCoroutine(DownloadFontStyle(styles[style], path), this);
+                        }
+                    }
+                }
+
+                // And show a download all button, to download all styles at once.
+                EditorGUILayout.Space();
+                if (GUILayout.Button("Download All Styles"))
+                {
+                    string path = EditorUtility.SaveFolderPanel("Save \"" + font.family + "\" to folder", "", "");
+                    if (path.Length != 0)
+                    {
+                        EditorCoroutineUtility.StartCoroutine(DownloadFont(path), this);
+                    }
+                }
+            }
+            EditorGUILayout.EndScrollView();
+            EditorGUILayout.EndVertical();
+        }
+
+        // Downloads the specified font style (from the given url) to the given
+        // file path.
+        IEnumerator DownloadFontStyle(string url, string filePath)
+        {
+            UnityWebRequest www = UnityWebRequest.Get(url);
+            yield return www.SendWebRequest();
+
+            if(www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                System.IO.File.WriteAllBytes(filePath, www.downloadHandler.data);
+                AssetDatabase.Refresh();
+            }
+        }
+
+        // Downloads all font styles to the given folder.
+        IEnumerator DownloadFont(string folderPath)
+        {
+            Dictionary<string, string> styles = font.GetStyles();
+            foreach (string style in styles.Keys)
+            {
+                string url = styles[style];
+                string path = folderPath + System.IO.Path.DirectorySeparatorChar + font.family + "(" + style + ").ttf";
+                yield return DownloadFontStyle(url, path);
+            }
+        }
+    }
+}
---
+++ Editor/GoogleFontsImporterWindow.cs
@@ -0,0 +1,144 @@
+// Copyright © Daniel Shervheim, 2020
+// www.danielshervheim.com
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+using EditorCoroutineUtility = Unity.EditorCoroutines.Editor.EditorCoroutineUtility;
+
+namespace DSS.GoogleFonts
+{
+    public class GoogleFontsImporterWindow : EditorWindow
+    {
+        // Delegate to draw contextual GUI.
+        delegate void GUI();
+        GUI drawGUI;
+
+        // Error message to display if an error occured.
+        string errorMessage = "";
+
+        // API call response object.
+        ApiResponse response = null;
+
+        // The search term.
+        string search = "";
+
+        // The category index.
+        int index = 0;
+
+        // The ccroll position in the font list.
+        Vector2 scrollPos = Vector2.zero;
+
+        [MenuItem("Google Fonts/Import")]
+        static void Window()
+        {
+            GoogleFontsImporterWindow window = (GoogleFontsImporterWindow)EditorWindow.GetWindow(typeof(GoogleFontsImporterWindow), false, "Google Fonts Importer");
+        }
+
+        void OnEnable()
+        {
+            drawGUI = ()=>{};
+
+            // Try and load the API key from the keyfile.
+            TextAsset keyFile = Resources.Load<TextAsset>("GoogleFontsApiKey");
+            if (keyFile == null)
+            {
+                errorMessage = "API key not found in\nAssets/GoogleFonts/Resources/GoogleFontsApiKey.txt";
+                drawGUI = Error;
+            }
+            else
+            {
+                EditorCoroutineUtility.StartCoroutine(MakeApiCall(keyFile.text), this);
+            }
+        }
+
+        IEnumerator MakeApiCall(string key)
+        {
+            drawGUI = Loading;
+
+            UnityWebRequest www = UnityWebRequest.Get("https://www.googleapis.com/webfonts/v1/webfonts?key=" + key);
+            yield return www.SendWebRequest();
+
+            if(www.isNetworkError || www.isHttpError)
+            {
+                errorMessage = "UnityWebRequest failed (" + www.responseCode + ")";
+                drawGUI = Error;
+            }
+            else
+            {
+                try
+                {
+                    response = ApiResponse.FromJson(www.downloadHandler.text);
+                    if (response.error.code != 0)
+                    {
+                        errorMessage = response.error.message;
+                        drawGUI = Error;
+                    }
+                    else
+                    {
+                        drawGUI = Success;
+                    }
+                }
+                catch(System.Exception e)
+                {
+                    errorMessage = "Json deserialization failed \n" + e.Message;
+                    drawGUI = Error;
+                }
+            }
+        }
+
+        // GUI functions.
+
+        void OnGUI()
+        {
+            drawGUI();
+        }
+
+        void Error()
+        {
+            EditorGUILayout.Space();
+            EditorGUILayout.HelpBox(errorMessage, MessageType.Error);
+        }
+
+        void Loading()
+        {
+            EditorGUILayout.Space();
+            EditorGUILayout.HelpBox("Loading...", MessageType.Info);
+        }
+
+        void Success()
+        {
+            EditorGUILayout.Space();
+
+            // Search box.
+            search = EditorGUILayout.TextField("Search", search);
+            EditorGUILayout.Space();
+
+            // Category.
+            index = EditorGUILayout.Popup(index, response.GetCategories(true));
+            EditorGUILayout.Space();
+
+            // Display list of fonts.
+            EditorGUILayout.BeginVertical();
+            scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+            {
+                List<GoogleFont> fonts = response.GetFontsBySearch(search, response.GetCategories()[index]);
+                foreach (GoogleFont font in fonts)
+                {
+                    // If they clicked on this font, spawn a new window to show
+                    // the individual styles available.
+                    if (GUILayout.Button(font.family))
+                    {
+                        GoogleFontWindow.Window(font);
+                        EditorGUILayout.Space();
+                    }
+                }
+            }
+            EditorGUILayout.EndScrollView();
+            EditorGUILayout.EndVertical();
+        }
+    }
+}
